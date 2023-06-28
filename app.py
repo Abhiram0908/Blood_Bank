2b81ae6f71c22354f14da2d9fb1c3221590af87f
Abhiram0908 <dacharlaabhiram0908@gmail.com>
2023-06-29 01:06:14 +0530

    made a homepage

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, url_for, request, session, redirect
-from flask_pymongo import PyMongo
-import bcrypt
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
 
+
+@app.route("/login")
+def login():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.secret_key = "secretkey"
