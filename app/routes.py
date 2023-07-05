4464d93f457d589727dfa082c08b9fef93d99416
Abhiram0908 <dacharlaabhiram0908@gmail.com>
2023-07-05 21:35:12 +0530

    enabled user to update details

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from app import app, db, bcrypt
-from app.forms import RegistrationForm, LoginForm
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from app.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -13,8 +13,8 @@ def home():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if  current_user.is_authenticated:
-        return redirect(url_for('home'))
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
@@ -34,27 +34,42 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if  current_user.is_authenticated:
-        return redirect(url_for('home'))
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data) 
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("home"))
         else:
             flash("Please check your Email or Password!", "danger")
     return render_template("login.html", title="Login", form=form)
+
 
 @app.route("/logout")
 def logout():
     logout_user()
     flash("You Loged out", "sucess")
-    return redirect(url_for('home'))
+    return redirect(url_for("home"))
 
-#put restrictions over some pages (must login)
-@app.route("/account")
-@ login_required
+
+# put restrictions over some pages (must login)
+@app.route("/account", methods=["GET", "POST"])
+@login_required
 def account():
-    return render_template("account.html", title="account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your Account has been Updated!", "sucess")
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
+    return render_template(
+        "account.html", title="Account", image_file=image_file, form=form
+    )
